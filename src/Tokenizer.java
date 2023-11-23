22cdf2fd7c216917e70410aaff3d3091c3a321ca
ferevr <l21550302@chihuahua2.tecnm.mx>
2023-11-23 16:38:00 -0600

    Error Lexico

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -1,159 +1,178 @@
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Tokenizer {
+
     private String inputString;
-    
-    private String DetectPitbull(String text){
+
+    private Boolean ErrorLexico(String text) {
+        Pattern patron = Pattern.compile("(\\d)*(\\w)*[=]*[<]*[>]*[ ]*[\n]*[\t]*");
+        Matcher matcher = patron.matcher(text);
+
+        if (matcher.find()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private String DetectPitbull(String text) {
         //DECLARAR VARIABLE
         Pattern patron = Pattern.compile("Pitbull\\s*[A-Za-z]+[0-9]*");
         Matcher matcher = patron.matcher(text);
-        if (matcher.find()){
+        if (matcher.find()) {
             return matcher.group();
         } else {
             return null;
         }
     }
-    
-    private String DetectChihuahua(String text){
+
+    private String DetectChihuahua(String text) {
         //IMPRIMIR
         Pattern patron = Pattern.compile("Chihuahua\\s*[A-Za-z]+[0-9]*");
         Matcher matcher = patron.matcher(text);
-        if (matcher.find()){
+        if (matcher.find()) {
             return matcher.group();
         } else {
             return null;
         }
     }
-    
-    private String DetectGolden(String text){
+
+    private String DetectGolden(String text) {
         //CONDICIONAL IF
         Pattern patron = Pattern.compile("GoldenRetriever\\s*[A-Za-z0-9]*(<|>|>=|<=|==|=!)[0-9]*");
         Matcher matcher = patron.matcher(text);
-        if (matcher.find()){
+        if (matcher.find()) {
             return matcher.group();
         } else {
             return null;
         }
     }
-    
-    private String DetectLabrador(String text){
+
+    private String DetectLabrador(String text) {
         //ARREGLO
         Pattern patron = Pattern.compile("Labrador\\s*=\\s*\\d*");
         Matcher matcher = patron.matcher(text);
-        if (matcher.find()){
+        if (matcher.find()) {
             return matcher.group();
         } else {
             return null;
         }
     }
-    
-    private String DetectHusky(String text){
+
+    private String DetectHusky(String text) {
         //BUCLE
         Pattern patron = Pattern.compile("Husky\\s*[A-Za-z0-9]*(<|>|>=|<=|==|=!)[0-9]*");
         Matcher matcher = patron.matcher(text);
-        
-        if (matcher.find()){
+
+        if (matcher.find()) {
             return matcher.group();
         } else {
             return null;
         }
     }
-    
+
     public void Analize(String inputString) {
-        if (DetectChihuahua(inputString) != null){
-            System.out.println("");
-            System.out.println("Lexema: Chihuahua");
-            System.out.println("Token: Instruccion");
-            Pattern Cpatron = Pattern.compile("(?<=Chihuahua\\s)[A-Za-z0-9]*");
-            Matcher matcher = Cpatron.matcher(inputString);
-            
-            if (matcher.find()){        
-                System.out.println("lexema: " + matcher.group());
+        if (ErrorLexico(inputString)) {
+            //True
+            System.err.println("hay un error lexico en el programa");
+        } else {
+            //False
+            if (DetectChihuahua(inputString) != null) {
+                System.out.println("");
+                System.out.println("Lexema: Chihuahua");
+                System.out.println("Token: Instruccion");
+                Pattern Cpatron = Pattern.compile("(?<=Chihuahua\\s)[A-Za-z0-9]*");
+                Matcher matcher = Cpatron.matcher(inputString);
+
+                if (matcher.find()) {
+                    System.out.println("lexema: " + matcher.group());
+                }
+                System.out.println("Token: Variable");
             }
-            System.out.println("Token: Variable");
+
+            if (DetectPitbull(inputString) != null) {
+                System.out.println("");
+                System.out.println("Lexema: Pitbull");
+                System.out.println("Token: Instruccion");
+                Pattern Cpatron = Pattern.compile("(?<=Pitbull\\s)[A-Za-z0-9]*");
+                Matcher matcher = Cpatron.matcher(inputString);
+
+                if (matcher.find()) {
+                    System.out.println("lexema: " + matcher.group());
+                }
+                System.out.println("Token: Variable");
+            }
+
+            if (DetectGolden(inputString) != null) {
+                System.out.println("");
+                System.out.println("Lexema: GoldenRetriever");
+                System.out.println("Token: Instruccion");
+                Pattern Gpatron = Pattern.compile("(?<=GoldenRetriever\\s)[A-Za-z0-9]*");
+                Matcher matcher = Gpatron.matcher(inputString);
+
+                if (matcher.find()) {
+                    System.out.println("lexema: " + matcher.group());
+                }
+                System.out.println("Token: Variable");
+                Pattern patron = Pattern.compile("(<[=]*)|(>[=]*)|(==)|(!=)");
+                Matcher match = patron.matcher(inputString);
+                if (match.find()) {
+                    System.out.println("lexema: " + match.group());
+                }
+                System.out.println("Token: Operador");
+
+                Pattern patron2 = Pattern.compile("(?<=(<[=]*)|(>[=]*)|(==)|(!=))[0-9]*");
+                Matcher match2 = patron2.matcher(inputString);
+                if (match2.find()) {
+                    System.out.println("Lexema: " + match2.group());
+                }
+                System.out.println("Token: Numero Entero");
+            }
+
+            if (DetectLabrador(inputString) != null) {
+                System.out.println("");
+                System.out.println("Lexema: Labrador");
+                System.out.println("Token: Instruccion");
+
+                System.out.println("Lexema: =");
+                System.out.println("Token: Operador");
+
+                Pattern patron2 = Pattern.compile("\\s\\d*");
+                Matcher match2 = patron2.matcher(inputString);
+                if (match2.find()) {
+                    System.out.println("Lexema: " + match2.group());
+                }
+                System.out.println("Token: Numero entero");
+            }
+
+            if (DetectHusky(inputString) != null) {
+                System.out.println("");
+                System.out.println("Lexema: Husky");
+                System.out.println("Token: Instruccion");
+
+                Pattern GGpatron = Pattern.compile("(?<=Husky\\s)[A-Za-z0-9]*");
+                Matcher matcher = GGpatron.matcher(inputString);
+
+                if (matcher.find()) {
+                    System.out.println("lexema: " + matcher.group());
+                }
+                System.out.println("Token: Variable");
+                Pattern patron = Pattern.compile("(<[=]*)|(>[=]*)|(==)|(!=)");
+                Matcher match = patron.matcher(inputString);
+                if (match.find()) {
+                    System.out.println("lexema: " + match.group());
+                }
+                System.out.println("Token: Operador");
+
+                Pattern patron2 = Pattern.compile("(?<=(<[=]*)|(>[=]*)|(==)|(!=))[0-9]*");
+                Matcher match2 = patron2.matcher(inputString);
+                if (match2.find()) {
+                    System.out.println("Lexema: " + match2.group());
+                }
+                System.out.println("Token: Numero Entero");
+            }
         }
-        
-        if (DetectPitbull(inputString) != null){
-            System.out.println("");
-            System.out.println("Lexema: Pitbull");
-            System.out.println("Token: Instruccion");
-            Pattern Cpatron = Pattern.compile("(?<=Pitbull\\s)[A-Za-z0-9]*");
-            Matcher matcher = Cpatron.matcher(inputString);
-            
-            if (matcher.find()){        
-                System.out.println("lexema: " + matcher.group());
-            }
-            System.out.println("Token: Variable");
-        }
-        
-        if (DetectGolden(inputString) != null){
-            System.out.println("");
-            System.out.println("Lexema: GoldenRetriever");
-            System.out.println("Token: Instruccion");
-            Pattern Gpatron = Pattern.compile("(?<=GoldenRetriever\\s)[A-Za-z0-9]*");
-            Matcher matcher = Gpatron.matcher(inputString);
-            
-            if (matcher.find()){        
-                System.out.println("lexema: " + matcher.group());
-            }
-            System.out.println("Token: Variable");
-            Pattern patron = Pattern.compile("(<[=]*)|(>[=]*)|(==)|(!=)");
-            Matcher match = patron.matcher(inputString);
-            if (match.find()){
-                System.out.println("lexema: " + match.group());
-            }
-            System.out.println("Token: Operador");
-            
-            Pattern patron2 = Pattern.compile("(?<=(<[=]*)|(>[=]*)|(==)|(!=))[0-9]*");
-            Matcher match2 = patron2.matcher(inputString);
-            if (match2.find()){
-                System.out.println("Lexema: " + match2.group());
-            }
-            System.out.println("Token: Numero Entero");
-        }
-        
-        if (DetectLabrador(inputString) != null){
-            System.out.println("");
-            System.out.println("Lexema: Labrador");
-            System.out.println("Token: Instruccion");
-            
-            System.out.println("Lexema: =");
-            System.out.println("Token: Operador");
-            
-            Pattern patron2 = Pattern.compile("\\s\\d*");
-            Matcher match2 = patron2.matcher(inputString);
-            if (match2.find()){
-                System.out.println("Lexema: " + match2.group());
-            }
-            System.out.println("Token: Numero entero");
-        }
-        
-        if (DetectHusky(inputString) != null){
-            System.out.println("");
-            System.out.println("Lexema: Husky");
-            System.out.println("Token: Instruccion");
-            
-            Pattern GGpatron = Pattern.compile("(?<=Husky\\s)[A-Za-z0-9]*");
-            Matcher matcher = GGpatron.matcher(inputString);
-            
-            if (matcher.find()){        
-                System.out.println("lexema: " + matcher.group());
-            }
-            System.out.println("Token: Variable");
-            Pattern patron = Pattern.compile("(<[=]*)|(>[=]*)|(==)|(!=)");
-            Matcher match = patron.matcher(inputString);
-            if (match.find()){
-                System.out.println("lexema: " + match.group());
-            }
-            System.out.println("Token: Operador");
-            
-            Pattern patron2 = Pattern.compile("(?<=(<[=]*)|(>[=]*)|(==)|(!=))[0-9]*");
-            Matcher match2 = patron2.matcher(inputString);
-            if (match2.find()){
-                System.out.println("Lexema: " + match2.group());
-            }
-            System.out.println("Token: Numero Entero");
-        }
-    }  
+    }
 }
